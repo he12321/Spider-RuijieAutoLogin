2d07248d3c9c5b22e670165f3579e006813bfe43
小贺 <3168738793@qq.com>
2023-02-05 21:59:24 +0800

    异步协程

--- 四线程/test.py
+++ 四线程/test.py
@@ -1,6 +1,31 @@
+import requests
+import json
+
 dic = {
     "你好": "hello",
-    "hi": "hi",
+    "嗨": "hi",
     "英语": "English"
 }
-print(dic["你好"])
+h = [1, 2, 3, 4]
+l = list(dic.keys())
+list = list(dic.values())
+
+date = {
+    "date": "12345"
+}
+url = "http://www.xinfadi.com.cn/getPriceData.html"
+data = {
+    "limit": 20,
+    "current": 2
+}
+resp = requests.post(url, data=data)
+html = resp.text
+html = json.loads(html)
+list = html["list"]
+i = 10086
+page = i
+data = {
+    "limit": 20,
+    "current": page
+}
+print(data)
